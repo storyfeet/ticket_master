78ce749fe4a3cc08cb00f92af1851ad20b8502ed
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-01-08 19:31:22 +0000

    managed to get database build before each test

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -3,12 +3,23 @@
 use Database\Seeders\OrderStatusSeeder;
 use Database\Seeders\TransactionStatusSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Ticket;
+use App\Models\User;
 
 uses(RefreshDatabase::class);
 
+beforeEach(function() {
+    $this->artisan( 'migrate --seed' );
+});
+
 test('database can be used',function(){
-    $this->migrate();
-    $this->seed();
     $response = $this->get("/tickets/open");
     $response->assertStatus(200);
 });
+
+test('seeded database has members',function(){
+    $qr = User::query()->select(['name','email'])->where('name','Test User')->get();
+    $this->assertEquals($qr->count(),1);
+    $this->assertEquals($qr[0]->email,'test@example.com');
+});
+
