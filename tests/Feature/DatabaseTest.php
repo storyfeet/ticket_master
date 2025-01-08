5b6d4076a4cbe5968ca653d059ce2bc3e35e78a2
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-01-08 19:54:53 +0000

    tested process random ticket

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -5,6 +5,7 @@ use Database\Seeders\TransactionStatusSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Ticket;
 use App\Models\User;
+use App\Console\Commands\ProcessTickets;
 
 uses(RefreshDatabase::class);
 
@@ -21,5 +22,16 @@ test('seeded database has members',function(){
     $qr = User::query()->select(['name','email'])->where('name','Test User')->get();
     $this->assertEquals($qr->count(),1);
     $this->assertEquals($qr[0]->email,'test@example.com');
+});
+
+test('tickets can update',function(){
+    $tp = new ProcessTickets();
+    for ($i = 0; $i < 9; $i +=1){
+        $tp->processRandomTicket();
+    }
+    $count = Ticket::query()->where('status',true)->count() ;
+    $this->assertEquals($count,9);
+    $count = Ticket::query()->where('status',false)->count() ;
+    $this->assertEquals($count,11);
 });
 
