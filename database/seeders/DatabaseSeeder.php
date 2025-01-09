9d84f6260958fef7c8e3b9dd493c2acf7f199412
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-01-09 11:50:57 +0000

    now seeds a roles table where admin has the role 'admin'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Ticket;
+use App\Models\Role;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -31,6 +32,12 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@tickets.com',
             'password' => Hash::make("adminadmin"),
         ]);
+        $admin = User::query()->where('name','=','admin')->get();
+        Role::factory()->create([
+            'user'=>$admin[0]->id,
+            'role'=>'admin',
+        ]);
+
         User::factory()
             ->count(30)
             ->create();
