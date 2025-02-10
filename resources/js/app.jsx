f5195bce7d582a35f16f5304a91af44ccf8d62a9
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-02-10 11:09:52 +0000

    install vite, try to get running, problem with mime-types, possibly not finding path

---
+++ resources/js/app.jsx
@@ -0,0 +1,14 @@
+import './bootstrap';
+import '../css/app.css';
+
+import ReactDOM from 'react-dom/client';
+//import Home from './Page/Home';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <>
+        <h1>A welcome page</h1>
+        <p>All about welcoming you to the site</p>
+    </>
+);
+
+
