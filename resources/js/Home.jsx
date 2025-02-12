aac5f073d81546718fe74a56a8531ee2fbbfd514
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-02-12 20:55:11 +0000

    add user state for login tracking, todo make form work ajax-style

--- resources/js/Home.jsx
+++ resources/js/Home.jsx
@@ -1,12 +1,14 @@
-
+import { useState } from "react";
 import TicketList from "./TicketList";
 import LoginForm from "./Login";
 
 export default function Home() {
+    let [user, setUser] = useState(null)
+
     return (
         <>
             <h1>Ticket Master</h1>
-            <LoginForm />
+            <LoginForm user={user} userSetter={setUser} />
             <TicketList />
         </>
     );
--- resources/js/Login.jsx
+++ resources/js/Login.jsx
@@ -1,10 +1,23 @@
 
-export default function LoginForm() {
+export default function LoginForm({ user, userSetter }) {
+
+    async function handleSubmit() {
+        login = await fetch();
+        //TODO
+
+    }
+
+    if (user === null) {
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <label>Email : <input type="text" name="email" /></label><br />
+                <label>Password : <input type="password" name="password" /></label>
+                <input type="submit" value="Login" />
+            </form>
+        );
+    }
     return (
-        <form >
-            <label>Email : <input type="text" name="email" /></label><br />
-            <label>Password : <input type="password" name="password" /></label>
-            <input type="submit" value="Login" />
-        </form>
+        <h2>Welcome {user.name}</h2>
     );
 }
