dd64b50e17b6eadd990cb9c22df5c1e4ac6bc070
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-02-14 14:14:12 +0000

    get form to manage csrf and call login

--- resources/js/Login.jsx
+++ resources/js/Login.jsx
@@ -9,8 +9,10 @@ export default function LoginForm({ user, userSetter }) {
 
     if (user === null) {
 
+        console.log("CSRF :", window.CSRF_TOKEN);
         return (
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} action="/loginjson" method="post">
+                <input type="hidden" name="_token" value={window.CSRF_TOKEN} />
                 <label>Email : <input type="text" name="email" /></label><br />
                 <label>Password : <input type="password" name="password" /></label>
                 <input type="submit" value="Login" />
