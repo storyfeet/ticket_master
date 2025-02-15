c4a9ae4769fa8611ed2a6ead4776f84793b90c02
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-02-15 16:59:18 +0000

    let admin request tickets by email address

--- resources/js/Panels.jsx
+++ resources/js/Panels.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import TicketList from "./TicketList";
 export function Panel({ user }) {
 
     let [basePath, basePathSetter] = useState(null);
     let [canGetUser, canGetUserSetter] = useState(false);
+    let [errs, errSetter] = useState(null)
 
     function baseSet(newPath, canGetUser = false) {
         return () => {
+            errSetter(null);
+
             basePathSetter(newPath);
             canGetUserSetter(canGetUser);
         }
@@ -14,21 +17,33 @@ export function Panel({ user }) {
 
     return (
         <>
-            {user.isAdmin && <AdminPanel baseSetter={baseSet} />}
+            {user.isAdmin && <AdminPanel baseSetter={baseSet} errs={errs} errSetter={errSetter} />}
             < UserPanel user={user} baseSetter={baseSet} />
             {basePath && <TicketList basePath={basePath} baseSetter={baseSet} canGetUser={canGetUser} />}
         </>
     );
 }
 
-export function AdminPanel({ baseSetter }) {
+export function AdminPanel({ baseSetter, errs, errSetter }) {
+    let emailRef = useRef();
 
+    function handleTicketsByEmail() {
+        let email = emailRef.current.value;
+        if (!email) {
+            errSetter({ "email": ["Please provide Email Address"] });
+            return;
+        }
+        baseSetter(`/admin/get_user_tickets/${email}`)();
 
+    }
     return (
         <div className="admin_panel">
             <button onClick={baseSetter("/admin/get_open", true)}>Open Tickets</button>
             <button onClick={baseSetter("/admin/get_closed", true)}>Closed Tickets</button>
-            <button >Tickets By Email</button>
+            <div>
+                {errs?.email && <label className="error">{errs.email}</label>}
+                <input className={errs?.email && "error"} ref={emailRef} /><button onClick={handleTicketsByEmail} >Tickets By Email</button>
+            </div>
         </div >
     );
 }
