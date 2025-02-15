b4057658580186b6fad197e8c0e92a065cf8619c
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-02-15 13:42:02 +0000

    adminController now can now return user status by email

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use App\Models\Ticket;
+use Illuminate\Validation\ValidationException;
 
 class AdminController extends Controller {
 
@@ -35,6 +36,20 @@ class AdminController extends Controller {
         $tickets = Ticket::query()
             ->join('users','tickets.user','=','users.id')
             ->where('status',true)
+            ->orderByDesc('tickets.updated_at')
+            ->select(TicketController::TICKETS_USER)
+            ->paginate(3);
+
+        return $tickets;
+    }
+
+    public function getUserTickets($email){
+
+
+//        $email = request()->get('email');
+        $tickets = Ticket::query()
+            ->join('users','tickets.user','=','users.id')
+            ->where('users.email','=',$email)
             ->orderByDesc('tickets.updated_at')
             ->select(TicketController::TICKETS_USER)
             ->paginate(3);
