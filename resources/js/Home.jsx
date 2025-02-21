f05a508932b4bcabd25b0b1e96e3b0545e08db51
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-02-21 17:10:51 +0000

    i18next translation now works on exactly one item

--- resources/js/Home.jsx
+++ resources/js/Home.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
-import TicketList from "./TicketList";
 import LoginForm from "./Login";
 import { Panel } from "./Panels";
+import { useTranslation } from "react-i18next";
+
 
 export default function Home() {
     let [user, setUser] = useState(window.USER_INFO)
+    let { t } = useTranslation();
 
     return (
         <>
             <h1>Ticket Master</h1>
+            <p>{t("greeting")}</p>
             <LoginForm user={user} userSetter={setUser} />
             {user && <Panel user={user} />}
         </>
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,7 @@ import Home from "./Home";
 
 import ReactDOM from 'react-dom/client';
 //import Home from './Page/Home';
+import "./i18n";
 
 ReactDOM.createRoot(document.getElementById('app')).render(
     <Home />
