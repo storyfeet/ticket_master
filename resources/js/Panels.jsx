7d9e0d593722747e810ddc6c4657dca05e6838e8
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-03-15 14:48:09 +0000

    feat(view,en): Let admin turn off user view.
    The user view was getting in the way of the ticket view.
    Also adds english Translation for relevant buttons.

--- resources/js/Panels.jsx
+++ resources/js/Panels.jsx
@@ -15,13 +15,12 @@ import {postCsrfJson} from "./loader.js";
 
 export function Panel({ user }) {
 
-
-
     let [basePath, basePathSetter] = useState(null);
     let [refreshTickets,refreshTicketsSetter] = useState(new Date().getTime());
     let [canGetUser, canGetUserSetter] = useState(false);
     let [errs, errSetter] = useState(null)
     let [displayMode, displaySetter] = useState(DISPLAY_MODE.NONE)
+    let [adminMode , adminModeSetter] = useState(ADMIN_DISPLAY.NONE);
     let [currentTicket, currentTicketSetter] = useState(null)
 
     function refresher(){
@@ -37,6 +36,7 @@ export function Panel({ user }) {
             canGetUserSetter(canGetUser);
             displaySetter(DISPLAY_MODE.TICKETS);
     }
+
     function goNewTicket() {
         displaySetter(DISPLAY_MODE.NEW_TICKET);
     }
@@ -51,8 +51,11 @@ export function Panel({ user }) {
         <>
             <UpdateView user={user} goEditTicket={goEditTicket}/>
             {errs && <ErrListView errs={errs} errSetter={errSetter} refresher={refresher} />}
-            {user.isAdmin && <AdminPanel goTickets={goTickets} errs={errs} errSetter={errSetter} />}
-            < UserPanel user={user} goTickets={goTickets} goNewTicket={goNewTicket} />
+            {user.isAdmin &&
+                <AdminPanel goTickets={goTickets} errs={errs} errSetter={errSetter}
+                mode={adminMode} modeSetter={adminModeSetter} displaySetter={displaySetter}/>}
+            {!user.isAdmin || adminMode === ADMIN_DISPLAY.ME &&
+                < UserPanel user={user} goTickets={goTickets} goNewTicket={goNewTicket} />}
             {basePath && displayMode === DISPLAY_MODE.TICKETS &&
                 <TicketList basePath={basePath} goEditTicket={goEditTicket}
                     goTickets={goTickets} canGetUser={canGetUser}
@@ -72,43 +75,52 @@ const ADMIN_DISPLAY = {
     CREATE_USER :1,
     EMAIL_FILTER : 2,
     ADVANCED_FILTER : 3,
+    ME : 4,
 };
 
-export function AdminPanel({ goTickets, errs, errSetter }) {
+export function AdminPanel({ goTickets, errs, errSetter,mode,modeSetter,displaySetter }) {
     let { t } = useTranslation();
-    let [display,displaySetter ] = useState(ADMIN_DISPLAY.NONE);
 
-    function toggleDisplay(mode){
-        if (display === mode){
-            displaySetter(ADMIN_DISPLAY.NONE);
+    function toggleMode(newMode){
+        if (mode === newMode){
+            modeSetter(ADMIN_DISPLAY.NONE);
         }else {
-            displaySetter(mode);
+            modeSetter(newMode);
         }
     }
     function qGoTickets(path){
         return ()=>{
-            displaySetter(ADMIN_DISPLAY.NONE);
+            modeSetter(ADMIN_DISPLAY.NONE);
             goTickets(path,true);
+        }
+    }
+    function modeAndClear(newMode){
+        return function(){
+            displaySetter(DISPLAY_MODE.NONE);
+            toggleMode(newMode);
         }
     }
     return (
         <div className="admin_panel">
+            <button onClick={modeAndClear(ADMIN_DISPLAY.ME)}>
+                {t(mode === ADMIN_DISPLAY.ME ? "btn-close_me_as_user": "btn-me_as_user")}
+            </button>
             <button onClick={qGoTickets("/admin/get_open" )}>
                 {t("open_tickets")}</button>
             <button onClick={qGoTickets("/admin/get_closed")}>
                 {t("closed_tickets")}</button>
-            <button onClick={()=>{toggleDisplay(ADMIN_DISPLAY.EMAIL_FILTER)}}>{
-                t(display=== ADMIN_DISPLAY.EMAIL_FILTER ? "btn-close_email_filter":"btn-go_email_filter")
+            <button onClick={modeAndClear(ADMIN_DISPLAY.EMAIL_FILTER)}>{
+                t(mode === ADMIN_DISPLAY.EMAIL_FILTER ? "btn-close_email_filter":"btn-go_email_filter")
             }</button>
-            <button onClick={()=>{toggleDisplay(ADMIN_DISPLAY.ADVANCED_FILTER)}}>{
-                t(display=== ADMIN_DISPLAY.ADVANCED_FILTER ? "btn-close_advanced_filter":"btn-go_advanced_filter")
+            <button onClick={modeAndClear(ADMIN_DISPLAY.ADVANCED_FILTER)}>{
+                t(mode=== ADMIN_DISPLAY.ADVANCED_FILTER ? "btn-close_advanced_filter":"btn-go_advanced_filter")
             }</button>
-            <button onClick={()=>{toggleDisplay(ADMIN_DISPLAY.CREATE_USER)}}>{
-                t(display=== ADMIN_DISPLAY.CREATE_USER ? "btn-close_create_user":"btn-go_create_user")
+            <button onClick={modeAndClear(ADMIN_DISPLAY.CREATE_USER)}>{
+                t(mode=== ADMIN_DISPLAY.CREATE_USER ? "btn-close_create_user":"btn-go_create_user")
             }</button>
-            {display === ADMIN_DISPLAY.CREATE_USER && <CreateUser />}
-            {display === ADMIN_DISPLAY.EMAIL_FILTER && <EmailFilter goTickets={goTickets} />}
-            {display === ADMIN_DISPLAY.ADVANCED_FILTER &&
+            {mode === ADMIN_DISPLAY.CREATE_USER && <CreateUser />}
+            {mode === ADMIN_DISPLAY.EMAIL_FILTER && <EmailFilter goTickets={goTickets} />}
+            {mode === ADMIN_DISPLAY.ADVANCED_FILTER &&
                 <AdvancedFilter goTickets={goTickets} errs={errs} errSetter={errSetter}/>}
         </div >
     );
